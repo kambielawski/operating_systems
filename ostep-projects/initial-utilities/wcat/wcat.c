a11214cc940498d92aafcde7c65886bd7dd4fd5f
Kam Bielawski <kamtb28@gmail.com>
2020-10-25 21:48:38 -0500

    wcat passes all tests

--- ostep-projects/initial-utilities/wcat/wcat.c
+++ ostep-projects/initial-utilities/wcat/wcat.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
+
+#define BUFFER_SIZE 500
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    fprintf(stderr, "Usage: ./wcat <filepath>");
-    exit(1);
+    if (argc == 1) {
+      exit(0);
+    } else {
+      fprintf(stderr, "Usage: ./wcat <filepath>");
+      exit(1);
+    }
   }
 
-  FILE *fp = fopen(argv[1], "r");
-  if (fp == NULL) {
-    fprintf(stderr, "Error opening file\n");
-    exit(1);
+  FILE *fp;
+  char *buffer = (char *) malloc(BUFFER_SIZE);
+
+  for (int i = 1; i < argc; i++) {
+    fp = fopen(argv[i], "r");
+    if (fp == NULL) {
+      printf("wcat: cannot open file\n");
+      exit(1);
+    }
+    while (fgets(buffer, BUFFER_SIZE, fp)) {
+      printf("%s", buffer);
+    }
+    fclose(fp);
   }
 
-  char *buffer;
-  
-  while (buffer = fgets(buffer, 1000, fp)) {
-    printf("%s", buffer);
-  }
-
-  fclose(fp);
-
+  free(buffer);
   return 0;
 }
