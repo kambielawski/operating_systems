62425660725b6719a35ca0427cdf2d1799f8657d
Kam Bielawski <kamtb28@gmail.com>
2020-11-01 21:03:24 -0600

    shell takes all args

--- process_api/shell.c
+++ process_api/shell.c
@@ -4,6 +4,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define MAX_ARGS 10
+
+char **getArgs(char *input, char delim, int size) {
+  char *word = strtok(input, " ");
+  char **ret = NULL;
+  int wordCounter = 0;
+  while(word) {
+    ret = realloc(ret, sizeof(char*) * ++wordCounter);
+
+    if (ret == NULL) {
+      printf("memory allocation failed\n");
+      exit(1);
+    }
+
+    ret[wordCounter-1] = word;
+
+    word = strtok(NULL, " ");
+  }
+  ret = realloc(ret, sizeof(char*) * (wordCounter+1));
+  ret[wordCounter] = NULL;
+  return ret;
+}
+
 int main(int argc, char *argv[]) {
   
   char *input;
@@ -11,18 +34,21 @@ int main(int argc, char *argv[]) {
   size_t characters;
 
   input = (char *) malloc(inputsize); 
+
   
   printf("Kam's Shell > ");
   characters = getline(&input, &inputsize, stdin);
   input[characters-1] = '\0';
   characters--;
+
+  char **inputToWords = getArgs(input, '\x20', characters);
   
   int rc = fork();
   if (rc < 0) {
-    printf("fork failed!!!\n");
+    fprintf(stderr, "failed to fork");
     exit(1);
   } else if (rc == 0) {
-    execlp(input, input, NULL);
+    execvp(inputToWords[0], inputToWords);
   } else {
     wait(NULL);
     if (!strncmp(input, strdup("quit"), 4)) {
